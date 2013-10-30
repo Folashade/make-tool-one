12930996b47e80801cd613731620afaf20f8c963
Folashade Okunubi <foo@andrew.cmu.edu>
2013-10-30 02:25:07 -0400

    confused

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 var pg = require('pg');
 
 /** // FOR HEROKU SERVER  //  **/
-var client = new pg.Client({
-	user: process.env.TODO_APP_DEV_USER || 'FOkunubi',
-	password: process.env.TODO_APP_DEV_PASSWORD || 'folashad3', 
-	database: process.env.TODO_APP_DEV_DATABASE || 'cobalt',
-	host: process.env.TODO_APP_DEV_HOST || 'localhost',
-	port: process.env.TODO_APP_DEV_PORT || 5000
-})
+// var client = new pg.Client({
+// 	user: process.env.TODO_APP_DEV_USER || 'FOkunubi',
+// 	password: process.env.TODO_APP_DEV_PASSWORD || 'folashad3', 
+// 	database: process.env.TODO_APP_DEV_DATABASE || 'cobalt',
+// 	host: process.env.TODO_APP_DEV_HOST || 'localhost',
+// 	port: process.env.TODO_APP_DEV_PORT || 5000
+// })
 
-client.connect(process.env.DATABASE_URL, function(err, client) {
-  var query = client.query('SELECT * FROM surveys');
+// client.connect(process.env.DATABASE_URL, function(err, client) {
+//   var query = client.query('SELECT * FROM surveys');
 
-  query.on('row', function(row) {
-    console.log(JSON.stringify(row));
-  });
-});
+//   query.on('row', function(row) {
+//     console.log(JSON.stringify(row));
+//   });
+// });
 
 // not working 
 
@@ -26,9 +26,9 @@ client.connect(process.env.DATABASE_URL, function(err, client) {
 // client.connect();
 
 /** // FOR HEROKU SERVER  // **/
-// var conString = "postgres://mpeyvkpeoywcaj:mQB_kCBkTaZCP-ct0OhCNl3zBO@ec2-54-225-102-116.compute-1.amazonaws.com:5432/d2d1mma7140cav";
-// var client = new pg.Client(conString);
-// client.connect();
+var conString = "postgres://mpeyvkpeoywcaj:mQB_kCBkTaZCP-ct0OhCNl3zBO@ec2-54-225-102-116.compute-1.amazonaws.com:5432/d2d1mma7140cav";
+var client = new pg.Client(conString);
+client.connect();
 
 /** // SETTING UP BACKEND // **/
 var express = require("express"); // imports express
